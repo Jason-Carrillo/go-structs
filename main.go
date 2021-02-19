7fe55b7f0f84c154b18431c77bd7214e79c4b322
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-19 13:45:38 -0700

    added 3rd declaration of structs

--- main.go
+++ main.go
@@ -9,9 +9,17 @@ type person struct {
 
 func main() {
 	//Ways to assign values to structs
+
+	//#1 Declare Struct
 	// alex := person{"Alex", "Anderson"}
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	// #2 Declare Struct
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	// #3 Declare Struct
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
 
 	fmt.Println(alex)
 	fmt.Println(alex.firstName)
