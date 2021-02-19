b6c2022c03fe917ab673174a9c37aa8d774c96b3
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-19 13:39:27 -0700

    added prints to show full struct information

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	//Ways to assign values to structs
+	// alex := person{"Alex", "Anderson"}
 
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	fmt.Println(alex)
+	fmt.Println(alex.firstName)
+	fmt.Println(alex.lastName)
 }
